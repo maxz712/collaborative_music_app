28db8d9662e915debfb97ca2bb188360fd6633a8
MingZ12345 <xinmingzhang1@gmail.com>
2023-07-14 23:01:51 -0400

    added votes to skip song backend logic

--- music_controller/frontend/src/components/MusicPlayer.js
+++ music_controller/frontend/src/components/MusicPlayer.js
@@ -9,6 +9,14 @@ export default class MusicPlayer extends Componet {
         super(props);
     }
 
+    skipSong() {
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'}
+        };
+        fetch('/spotify/skip', requestOptions);
+    }
+
     pauseSong(){
         const requestOptions = {
             method: 'PUT',
@@ -43,7 +51,8 @@ export default class MusicPlayer extends Componet {
                         <IconButton onClick={() => {this.props.is_playing ? this.pauseSong(): this.playSong();}}>
                             {this.props.is_playing ? <PauseIcon/>:<PlayArrowIcon/>}
                         </IconButton>
-                        <IconButton>
+                        <IconButton onClick={() => this.skipSong()}>
+                            {this.props.votes} / {" "} {this.props.votes_required}
                             <SkipNextIcon/>
                         </IconButton>
                     </div>
