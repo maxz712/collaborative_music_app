eb9ad129cb4609aa4442a086c0792a5a1917ed68
MingZ12345 <xinmingzhang1@gmail.com>
2023-07-14 21:15:28 -0400

    added play and pause song feature

--- music_controller/frontend/src/components/MusicPlayer.js
+++ music_controller/frontend/src/components/MusicPlayer.js
@@ -9,6 +9,22 @@ export default class MusicPlayer extends Componet {
         super(props);
     }
 
+    pauseSong(){
+        const requestOptions = {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+        };
+        fetch('/spotify/pause', requestOptions);
+    }
+
+    playSong(){
+        const requestOptions = {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+        };
+        fetch('/spotify/play', requestOptions);
+    }
+
     render() {
         const songProgress = (this.props.time / this.props.duration) * 100;
         return(<Card>
@@ -24,7 +40,7 @@ export default class MusicPlayer extends Componet {
                         {this.props.artist}
                     </Typography>
                     <div>
-                        <IconButton>
+                        <IconButton onClick={() => {this.props.is_playing ? this.pauseSong(): this.playSong();}}>
                             {this.props.is_playing ? <PauseIcon/>:<PlayArrowIcon/>}
                         </IconButton>
                         <IconButton>
